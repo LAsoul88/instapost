a50296ec75c09428d9a6d75d156a2d6a24e70d42
LAsoul88 <84427378+LAsoul88@users.noreply.github.com>
2021-07-21 14:45:58 -0700

    established index route with dummy response

--- server.js
+++ server.js
@@ -13,7 +13,33 @@ const PORT = 4000;
 
 /* === Routes === */
 
+app.get("/", function (req, res) {
+    res.redirect("/posts");
+});
+
+// === Post
+
+// Index GET / - Presentational 
+app.get('/', (req, res) => {
+    res.send('Post Index');
+});
+
+// New GET /posts/new - Presentational Form
+
+// Create POST /posts - Functional
+
+// Show GET /posts/:id - Presentational
+
+// Edit GET /posts/:id/edit - Presentational Form
+
+// Update PUT /posts/:id - Functional
+
+// Destroy DELETE /posts/:id - Functional
+
 /* === Server Bind === */
 app.listen(PORT, () => {
-    console.log(`Instapost is live at port ${PORT} and ready to "borrow" your personal data`);
+    console.log(
+        `Instapost is live at port ${PORT} and ready to "borrow" your personal data`
+        );
 });
+
